1e595238e61717dc0d9d848ac84636adecd0e60c
Jonas Heinrich <j.heinrich@finn.de>
2015-01-29 10:51:13 +0100

    Update stable to v4.0.6

--- vendored-plugins/reporting_engine/lib/widget/controls/clear.rb
+++ vendored-plugins/reporting_engine/lib/widget/controls/clear.rb
@@ -17,12 +17,12 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #++
 
-
 class Widget::Controls::Clear < Widget::Controls
   def render
-    html = link_to(content_tag(:span, content_tag(:em, l(:"button_clear"), :class => "button-icon icon-clear")),
-                  '#', :id => 'query-link-clear', :class => 'button secondary')
-    write html
+    write link_to(l(:button_clear),
+                  '#',
+                  id: 'query-link-clear',
+                  class: 'button icon-context icon-undo')
     maybe_with_help
   end
 end
--- vendored-plugins/reporting_engine/lib/widget/controls/save_as.rb
+++ vendored-plugins/reporting_engine/lib/widget/controls/save_as.rb
@@ -21,14 +21,12 @@ class Widget::Controls::SaveAs < Widget::Controls
   def render
     if @subject.new_record?
       link_name = l(:button_save)
-      icon = "icon-save"
+      icon = 'icon-save1'
     else
       link_name = l(:button_save_as)
-      icon = "icon-save-as"
+      icon = 'icon-save1'
     end
-    button = link_to content_tag(:span, content_tag(:em, link_name, :class => "button-icon icon-save-as")), "#",
-        :class => "button secondary",
-        :id => 'query-icon-save-as', :title => link_name
+    button = link_to(link_name, '#', id: 'query-icon-save-as', class: "button icon-context #{icon}")
     write(button + render_popup)
     maybe_with_help
   end
@@ -40,12 +38,12 @@ class Widget::Controls::SaveAs < Widget::Controls
   def render_popup_form
     name = content_tag :p do
       label_tag(:query_name, required_field_name(Query.human_attribute_name(:name))) +
-      text_field_tag(:query_name, @subject.name, :required => true)
+      text_field_tag(:query_name, @subject.name, required: true)
     end
     if @options[:can_save_as_public]
       box = content_tag :p do
         label_tag(:query_is_public, Query.human_attribute_name(:is_public)) +
-          check_box_tag(:query_is_public)
+        check_box_tag(:query_is_public)
       end
       name + box
     else
@@ -55,19 +53,22 @@ class Widget::Controls::SaveAs < Widget::Controls
 
   def render_popup_buttons
     content_tag(:p) do
-      save = link_to content_tag(:span, content_tag(:em, l(:button_save))), "#",
-        :id => "query-icon-save-button",
-        :class => "button reporting_button save",
-        :"data-target" => url_for(:action => 'create', :set_filter => '1')
-      cancel = link_to l(:button_cancel), "#",
-        :id => "query-icon-save-as-cancel",
-        :class => 'icon icon-cancel'
+      save = link_to(l(:button_save),
+                     '#',
+                     id: 'query-icon-save-button',
+                     class: 'button_highlight icon-context icon-save1',
+                     :"data-target" => url_for(action: 'create', set_filter: '1'))
+
+      cancel = link_to(l(:button_cancel),
+                       '#',
+                       id: 'query-icon-save-as-cancel',
+                       class: 'button icon-context icon-cancel')
       save + cancel
     end
   end
 
   def render_popup
-    content_tag :div, :id => 'save_as_form', :class => "button_form", :style => "display:none" do
+    content_tag :div, id: 'save_as_form', class: 'button_form', style: 'display:none' do
       render_popup_form + render_popup_buttons
     end
   end
